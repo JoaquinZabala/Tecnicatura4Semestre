2ce78de2b13eba2df83b750234c6327e26a9a6ae
JoaquinZabala <joacozabala21@gmail.com>
2023-10-30 23:42:45 -0300

    Proyectos Node
    Trabajos de node

---
+++ Node/01-Hola-Mundo/entorno.js
@@ -0,0 +1,8 @@
+
+let nombre = process.env.NOMBRE || 'Sin Nombre';
+let web = process.env.MI_WEB || 'No tengo web';
+
+console.log('Hola '+nombre);
+console.log('Mi web es: '+web);
+
+console.log('Ultima parte');
